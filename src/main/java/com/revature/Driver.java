716b09fd0c57ff5ace990a5c8b2057bce71f6b4a
pl9ed <pl9ed@virginia.edu>
2020-09-22 02:50:22 -0400

    commented out bethel's users in Driver for now, getting Null Pointer

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -7,20 +7,20 @@ import com.revature.services.UserService;
 public class Driver {
 
 	public static void main(String[] arg) {
-		UserService us = new UserService();
-		
-		us.register("train","toot","Thomas","Engine","ILoveTrains@gmail.com");
-		
-		User user = us.login("train", "toot");
-		
-		us.updateFirstName(user, "MC Queen");
-		System.out.println(user.toString());
-		
-		us.register("meme", "god", "dio", "brando", "Jojo@email.com");
-		
-		User user2 = us.login("meme", "god");
-		us.updateLastName(user2,"Stando");
-		us.updateEmail(user2, "ZaWorld@gmail.com");
+//		UserService us = new UserService();
+//		
+//		us.register("train","toot","Thomas","Engine","ILoveTrains@gmail.com");
+//		
+//		User user = us.login("train", "toot");
+//		
+//		us.updateFirstName(user, "MC Queen");
+//		System.out.println(user.toString());
+//		
+//		us.register("meme", "god", "dio", "brando", "Jojo@email.com");
+//		
+//		User user2 = us.login("meme", "god");
+//		us.updateLastName(user2,"Stando");
+//		us.updateEmail(user2, "ZaWorld@gmail.com");
 		
 		// init users for front end
 		User u1 = new User();
