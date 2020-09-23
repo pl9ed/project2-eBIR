1acad87a25de272ced5b717df485e65600fbe1d3
pl9ed <pl9ed@virginia.edu>
2020-09-23 12:11:18 -0400

    removed unnecessary junit dependency

--- src/test/java/com/revature/DAO/ReviewDAOTest.java
+++ src/test/java/com/revature/DAO/ReviewDAOTest.java
@@ -11,13 +11,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.revature.TestUtilities;
 import com.revature.models.Review;
