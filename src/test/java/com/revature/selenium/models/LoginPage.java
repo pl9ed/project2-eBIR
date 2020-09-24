d9eeca97079201a859bad1278c25409a53d671af
pl9ed <pl9ed@virginia.edu>
2020-09-24 19:17:05 -0400

    basic setup

--- src/test/java/com/revature/selenium/models/LoginPage.java
+++ src/test/java/com/revature/selenium/models/LoginPage.java
@@ -1,5 +1,20 @@
 package com.revature.selenium.models;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+	private static final String base_url = "http://52.205.93.132:8006/eBIRProject";
+	private WebDriver webdriver;
+	
+	private WebElement usernameInput;
+	private WebElement passInput;
+	private WebElement loginBtn;
+	private WebElement regBtn;
+	
+	public LoginPage(WebDriver wd) {
+		this.webdriver = wd;
+		wd.get(base_url);
+	}
 
 }
