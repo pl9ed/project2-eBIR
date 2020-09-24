dfed57313f8d3bad6f488dc74c713f4e5cec7d07
pl9ed <pl9ed@virginia.edu>
2020-09-24 15:13:21 -0400

    forgot to save hibernate util

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -13,12 +13,12 @@ public class HibernateUtil {
 	// check this if you can't connect to your DB or if HibernateUtil isn't working
 	static {
 		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		//cfg.setProperty("hibernate.connection.url", System.getenv("db_url"));
-//		cfg.setProperty("hibernate.connection.password", System.getenv("postgres_pw"));
-//		cfg.setProperty("hibernate.connection.username", System.getenv("postgres_username"));
+		cfg.setProperty("hibernate.connection.url", System.getenv("db_url"));
+		cfg.setProperty("hibernate.connection.password", System.getenv("postgres_pw"));
+		cfg.setProperty("hibernate.connection.username", System.getenv("postgres_username"));
 		
 		// replaced so that we can use the same code for our deployed app
-		// cfg.setProperty("hibernate.default_schema", System.getenv("project2_schema"));
+		 cfg.setProperty("hibernate.default_schema", System.getenv("project2_schema"));
 
 		try {
 			sf = cfg.buildSessionFactory();
