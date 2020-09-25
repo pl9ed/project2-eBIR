9f0ab04e3cd1b27519f9de81f27183e7976358ed
Nawlej <churknam@gmail.com>
2020-09-25 10:35:50 -0700

    updated UpdateTest.java, has issue

--- src/test/java/com/revature/selenium/UpdateTest.java
+++ src/test/java/com/revature/selenium/UpdateTest.java
@@ -1,10 +1,13 @@
 package com.revature.selenium;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,10 +19,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 class UpdateTest {
 
 	private static WebDriver driver;
+	private static String newFirstName;
+	private static String newLastName;
+	private static String newPassword;
+	private static String newEmail;
 
 	@BeforeAll
 	static void beforeClass() {
-		//log in as a:a
+		//log in as b:b
 		File file = new File("src/test/resources/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		driver = new ChromeDriver();
@@ -29,8 +36,8 @@ class UpdateTest {
 		
 		WebElement username = driver.findElement(By.id("username"));
 		WebElement password = driver.findElement(By.id("password"));
-		username.sendKeys("a");
-		password.sendKeys("a");
+		username.sendKeys("b");
+		password.sendKeys("b");
 	
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);	
 		WebElement loginBtn = driver.findElement(By.name("login"));
@@ -44,14 +51,16 @@ class UpdateTest {
 		WebElement updateBtn = driver.findElement(By.name("update"));
 		updateBtn.click();
 		
+		newFirstName = "newFirtName";
+		newLastName = "newLastName";
+		newPassword = "newPassword";
+		newEmail = "newEmail";
 	}
 	
 	@AfterAll
 	static void afterClass() {
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);	
-		WebElement updateBtn = driver.findElement(By.id("updateProfileBtn"));
-		updateBtn.click();
-		driver.quit();
+
+		//driver.quit();
 	}
 	
 	@Before
@@ -64,28 +73,45 @@ class UpdateTest {
 		
 	}
 	
-	@Test
-	void updateFirstName() {
+	@Ignore
+	void updateFirstName(String value) {
 		WebElement firstname = driver.findElement(By.id("uFirstname"));	
-		firstname.sendKeys("new 1st name");
+		firstname.sendKeys(value);
 	}
 	
-	@Test
-	void updateLastName() {
+	@Ignore
+	void updateLastName(String value) {
 		WebElement lastname = driver.findElement(By.id("uLastname"));
-		lastname.sendKeys("new last name");
+		lastname.sendKeys(value);
 	}
 	
-	@Test
-	void updatePassword() {
+	@Ignore
+	void updatePassword(String value) {
 		WebElement password = driver.findElement(By.id("uPassword"));
-		password.sendKeys("newpassword");
+		password.sendKeys(value);
+	}
+	
+	@Ignore
+	void updateEmail(String value) {
+		WebElement email = driver.findElement(By.id("uEmail"));
+		email.sendKeys(value);
 	}
 	
 	@Test
-	void updateEmail() {
-		WebElement email = driver.findElement(By.id("uEmail"));
-		email.sendKeys("newEmail@gmail.com");
+	void updateAllFields() {
+		updateEmail(newEmail);
+		updateFirstName(newFirstName);
+		updateLastName(newLastName);
+		updatePassword(newPassword);
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);	
+		WebElement updateBtn = driver.findElement(By.id("updatBtn"));
+		updateBtn.click(); //button doesn't work, do I need to use pageFactory???
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);	
+		String fullname = driver.findElement(By.id("fullname")).getText();
+
+		assertEquals(newFirstName+" "+newLastName, fullname);
 	}
 
 }
