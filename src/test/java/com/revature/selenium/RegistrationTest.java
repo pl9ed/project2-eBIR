72b2f7133b28df6372bd877e8e566f16aa362917
Nawlej <churknam@gmail.com>
2020-09-24 18:29:26 -0700

    added test to RegistrationTesst.java

--- src/test/java/com/revature/selenium/RegistrationTest.java
+++ src/test/java/com/revature/selenium/RegistrationTest.java
@@ -2,11 +2,18 @@ package com.revature.selenium;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 class RegistrationTest {
 
@@ -27,8 +34,33 @@ class RegistrationTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void registerTest() {
+		File file = new File("src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+	
+		//registering a new user with Hot wheels as username and password
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://52.205.93.132:8006/eBIRProject/#/login");
+		WebElement toRegisterBtn = driver.findElement(By.name("toRegister"));
+		toRegisterBtn.click();
+		
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement passwordConfirm = driver.findElement(By.id("confirmpassword"));
+		WebElement firstname = driver.findElement(By.id("firstname"));
+		WebElement lastname = driver.findElement(By.id("lastname"));
+		WebElement email = driver.findElement(By.id("email"));
+		
+		username.sendKeys("Hot");
+		password.sendKeys("Wheels");
+		passwordConfirm.sendKeys("Wheels");
+		firstname.sendKeys("Mario");
+		lastname.sendKeys("Mario");
+		email.sendKeys("nintendo@gmail.com");
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);	
+		WebElement registerBtn = driver.findElement(By.id("register"));
+		registerBtn.click(); 
 	}
 
 }
